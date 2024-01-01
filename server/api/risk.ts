461bb02f5a36ac9237a1fea9c062d6135bf8ef3b
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2024-01-01 09:21:04 -0500

    ✨ feat: API Improving

--- server/api/risk.ts
+++ server/api/risk.ts
@@ -2,8 +2,11 @@ export default defineEventHandler(async (event) => {
   type DangersData = {
     city?: string;
     citycode?: string;
-    // azi?: any;
-    // catnat?: any;
+    azi?: any;
+    catnat?: any;
+    cavite?: any;
+    mvt?: any;
+    radon?: any;
     risques?: any;
   };
   const dangerData: DangersData = {};
@@ -18,50 +21,50 @@ export default defineEventHandler(async (event) => {
     dangerData.citycode = data.features[0].properties.citycode;
 
     if (dangerData.citycode !== undefined) {
-      // // AZI request
-      // let urlAZI = `https://www.georisques.gouv.fr/api/v1/gaspar/azi?code_insee=${dangerData.citycode}&page=1&page_size=10&rayon=1000`;
-      // const responseAZI = await fetch(urlAZI);
-      // if (!responseAZI.ok) {
-      //   throw new Error(`Request failed with status ${responseAZI.status}`);
-      // }
-      // const dataAZI = await responseAZI.json();
-      // console.log(dataAZI.data);
+      // AZI request
+      let urlAZI = `https://www.georisques.gouv.fr/api/v1/gaspar/azi?code_insee=${dangerData.citycode}&page=1&page_size=10&rayon=1000`;
+      const responseAZI = await fetch(urlAZI);
+      if (!responseAZI.ok) {
+        throw new Error(`Request failed with status ${responseAZI.status}`);
+      }
+      const dataAZI = await responseAZI.json();
+      dangerData.azi = dataAZI.data[0];
 
-      // // CATNAT request
-      // let urlCATNAT = `https://www.georisques.gouv.fr/api/v1/gaspar/catnat?code_insee=${dangerData.citycode}&page=1&page_size=10&rayon=1000`;
-      // const responseCATNAT = await fetch(urlCATNAT);
-      // if (!responseCATNAT.ok) {
-      //   throw new Error(`Request failed with status ${responseCATNAT.status}`);
-      // }
-      // const dataCATNAT = await responseCATNAT.json();
-      // console.log(dataCATNAT.data);
+      // CATNAT request
+      let urlCATNAT = `https://www.georisques.gouv.fr/api/v1/gaspar/catnat?code_insee=${dangerData.citycode}&page=1&page_size=10&rayon=1000`;
+      const responseCATNAT = await fetch(urlCATNAT);
+      if (!responseCATNAT.ok) {
+        throw new Error(`Request failed with status ${responseCATNAT.status}`);
+      }
+      const dataCATNAT = await responseCATNAT.json();
+      dangerData.catnat = dataCATNAT.data;
 
-      // // CAVITE request
-      // let urlCAVITE = `https://www.georisques.gouv.fr/api/v1/cavites?code_insee=${dangerData.citycode}&page=1&page_size=10`;
-      // const responseCAVITE = await fetch(urlCAVITE);
-      // if (!responseCAVITE.ok) {
-      //   throw new Error(`Request failed with status ${responseCAVITE.status}`);
-      // }
-      // const dataCAVITE = await responseCAVITE.json();
-      // console.log(dataCAVITE.data);
+      // CAVITE request
+      let urlCAVITE = `https://www.georisques.gouv.fr/api/v1/cavites?code_insee=${dangerData.citycode}&page=1&page_size=10`;
+      const responseCAVITE = await fetch(urlCAVITE);
+      if (!responseCAVITE.ok) {
+        throw new Error(`Request failed with status ${responseCAVITE.status}`);
+      }
+      const dataCAVITE = await responseCAVITE.json();
+      dangerData.cavite = dataCAVITE.data;
 
-      // // MVT request
-      // let urlMVT = `https://www.georisques.gouv.fr/api/v1/mvt?code_insee=${dangerData.citycode}&page=1&page_size=10`;
-      // const responseMVT = await fetch(urlMVT);
-      // if (!responseMVT.ok) {
-      //   throw new Error(`Request failed with status ${responseMVT.status}`);
-      // }
-      // const dataMVT = await responseMVT.json();
-      // console.log(dataMVT.data);
+      // MVT request
+      let urlMVT = `https://www.georisques.gouv.fr/api/v1/mvt?code_insee=${dangerData.citycode}&page=1&page_size=10`;
+      const responseMVT = await fetch(urlMVT);
+      if (!responseMVT.ok) {
+        throw new Error(`Request failed with status ${responseMVT.status}`);
+      }
+      const dataMVT = await responseMVT.json();
+      dangerData.mvt = dataMVT.data;
 
-      // // RADON request
-      //   let urlRADON = `https://www.georisques.gouv.fr/api/v1/radon?code_insee=${dangerData.citycode}&page=1&page_size=10`;
-      //   const responseRADON = await fetch(urlRADON);
-      //   if (!responseRADON.ok) {
-      //     throw new Error(`Request failed with status ${responseRADON.status}`);
-      //   }
-      //   const dataRADON = await responseRADON.json();
-      //   console.log(dataRADON.data);
+      // RADON request
+      let urlRADON = `https://www.georisques.gouv.fr/api/v1/radon?code_insee=${dangerData.citycode}&page=1&page_size=10`;
+      const responseRADON = await fetch(urlRADON);
+      if (!responseRADON.ok) {
+        throw new Error(`Request failed with status ${responseRADON.status}`);
+      }
+      const dataRADON = await responseRADON.json();
+      dangerData.radon = dataRADON.data[0];
 
       // RISQUE request
       let urlRISQUE = `https://www.georisques.gouv.fr/api/v1/gaspar/risques?code_insee=${dangerData.citycode}&page=1&page_size=10`;
@@ -70,8 +73,8 @@ export default defineEventHandler(async (event) => {
         throw new Error(`Request failed with status ${responseRISQUE.status}`);
       }
       const dataRISQUE = await responseRISQUE.json();
-      dangerData.risques = dataRISQUE.data[0];
-      console.log(dangerData);
+      dangerData.risques = dataRISQUE.data[0].risques_detail;
+
       return dangerData;
     }
   } catch (error) {
