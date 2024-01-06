759f7287d297f7e877dc88ce54cffa8ef8cca7e4
p2101799 <antoine.voillot@etu.univ-lyon1.fr>
2024-01-06 15:49:05 -0500

    ✨ feat: Risk Component

---
+++ components/risks/risks-label.vue
@@ -0,0 +1,31 @@
+<template>
+  <p
+    class="bg-green-300 text-green-700 font-semibold rounded-lg w-fit pl-3 pr-3"
+    v-if="level == 0"
+  >
+    FAIBLE
+  </p>
+  <p
+    class="bg-yellow-300 text-yellow-500 font-semibold rounded-lg w-fit pl-3 pr-3"
+    v-else-if="level == 1"
+  >
+    EXISTANT
+  </p>
+  <p
+    class="bg-red-300 text-red-500 font-semibold rounded-lg w-fit pl-3 pr-3"
+    v-else-if="level >= 2"
+  >
+    IMPORTANT
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ components/risks/risks-war-engine.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="bg-white risk">
+    <img
+      src="../../assets/svg/risksIcons/WarEngine.svg"
+      alt="Avalanche"
+      class="icon"
+    />
+    <div class="text-black flex flex-col items-center">
+      <h3 class="text-center text-xl font-semibold">Engins de Guerre</h3>
+      <RisksLabel :level="level" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.risk {
+  width: 20vw;
+  position: relative; /* Ajout de la propriété position relative */
+  border-radius: 10px;
+  padding: 3vw 1vw 1vw 1vw;
+  margin-top: 3vw;
+}
+
+.icon {
+  width: 5vw;
+  height: 5vw;
+  position: absolute;
+  top: 0; /* Positionne l'image au sommet de la div */
+  left: 50%; /* Centre l'image horizontalement */
+  transform: translate(
+    -50%,
+    -50%
+  ); /* Décale l'image de 50% de sa propre largeur vers la gauche */
+}
+
+.content {
+  color: black;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    level: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
